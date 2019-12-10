90a1406d9de02141fd300c63172c9b42a7280f3b
dill <veltmeyer.s@gmail.com>
2019-12-10 15:20:37 -0800

    spring boot init

---
+++ src/main/java/com/example/simplespringbootwsclient/SimpleSpringbootWsClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootwsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootWsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringbootWsClientApplication.class, args);
+	}
+
+}
